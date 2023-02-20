10013087e6b72113eadc20397fdf1fc329955852
Bernardo-MG <programming@bernardo-mg.com>
2023-02-20 09:23:21 +0100

    Added properties class

--- src/main/java/com/bernardomg/example/tcp/spring/config/TcpConfig.java
+++ src/main/java/com/bernardomg/example/tcp/spring/config/TcpConfig.java
@@ -1,15 +1,17 @@
 
 package com.bernardomg.example.tcp.spring.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.bernardomg.example.tcp.spring.property.TcpProperties;
 import com.bernardomg.example.tcp.spring.server.ReactorNettyTcpServer;
 import com.bernardomg.example.tcp.spring.server.Server;
 import com.bernardomg.example.tcp.spring.server.handler.EchoResponseHandler;
 
 @Configuration
+@EnableConfigurationProperties(TcpProperties.class)
 public class TcpConfig {
 
     public TcpConfig() {
@@ -17,8 +19,8 @@ public class TcpConfig {
     }
 
     @Bean(name = "tcpServer", initMethod = "start", destroyMethod = "close")
-    public Server getTcpServer(@Value("${tcp.port}") final Integer port) {
-        return new ReactorNettyTcpServer(port, new EchoResponseHandler());
+    public Server getTcpServer(final TcpProperties properties) {
+        return new ReactorNettyTcpServer(properties.getPort(), new EchoResponseHandler());
     }
 
 }
